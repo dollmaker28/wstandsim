1c26193820ccc37cf49c00035b8d04f9fc11f668
m.schroe <m.schroe@outlook.de>
2018-03-19 15:47:34 +0100

    Buttons für Verkauf inkl. Funktionalität eingefügt.

--- WStandSim/WStandSim/Helpers/Simulation.cs
+++ WStandSim/WStandSim/Helpers/Simulation.cs
@@ -28,5 +28,66 @@ namespace WStandSim.Helpers
 
         // Ausgabe der Wettervorhersage
         public string ReturnWeather { get { return db.WeatherForecast(); } }
+
+        // Properties für die Zähler auf der Verkaufsseite
+        #region PropsArtikel
+        // Variablen
+        public int sausages, bread, beer, lemonades;
+        // Properties
+        public int Sausages
+        { get { return sausages; } set { sausages = value; NotifyPropertyChanged(); } }
+        public int Bread
+        { get { return bread; } set { bread = value; NotifyPropertyChanged(); } }
+        public int Beer
+        { get { return beer; } set { beer = value; NotifyPropertyChanged(); } }
+        public int Lemonades
+        { get { return lemonades; } set { lemonades = value; NotifyPropertyChanged(); } }
+        #endregion
+
+        // Methode zum Erhöhen und Reduzieren der Artikel auf der Verkaufsseite
+        // Wurst
+        public void RaiseSausages()
+        {
+            int a = sausages;
+            Sausages = a + 1;
+        }
+        public void LowerSausages()
+        {
+            int a = sausages;
+            if (a == 0) { } else Sausages = a - 1;
+        }
+        // Brot
+        public void RaiseBread()
+        {
+            int a = bread;
+            Bread = a + 1;
+        }
+        public void LowerBread()
+        {
+            int a = bread;
+            if (a == 0) { } else Bread = a - 1;
+        }
+        // Bier
+        public void RaiseBeer()
+        {
+            int a = beer;
+            Beer = a + 1;
+        }
+        public void LowerBeer()
+        {
+            int a = beer;
+            if (a == 0) { } else Beer = a - 1;
+        }
+        // Limonade
+        public void RaiseLemonades()
+        {
+            int a = lemonades;
+            Lemonades = a + 1;
+        }
+        public void Loweremonades()
+        {
+            int a = lemonades;
+            if (a == 0) { } else Lemonades = a - 1;
+        }
     }
 }
--- WStandSim/WStandSim/Views/Stock.xaml.cs
+++ WStandSim/WStandSim/Views/Stock.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WStandSim.Helpers;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,20 +12,64 @@ namespace WStandSim
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Stock : ContentPage
 	{
-		public Stock ()
+        // Instanzierung
+        Simulation s;
+
+        // Initialisieren
+        public Stock ()
 		{
 			InitializeComponent ();
-		}
-
-        private void Button_ClickedCheckout(object sender, EventArgs e)
-        {
-
+            s = new Simulation();
+            BindingContext = s;
         }
 
+        // Zurück
         private void Button_ClickedOverview(object sender, EventArgs e)
         {
             base.OnBackButtonPressed();
             //await Navigation.PushModalAsync(new Overview());
+        }
+
+        // Buttons für Ein - Verkauf
+        #region UpDownButtons
+        private void Button_ClickedSausageUP(object sender, EventArgs e)
+        {
+            s.RaiseSausages();
+        }
+        private void Button_ClickedSausageDOWN(object sender, EventArgs e)
+        {
+            s.LowerSausages();
+        }
+        private void Button_ClickedBreadUP(object sender, EventArgs e)
+        {
+            s.RaiseBread();
+        }
+        private void Button_ClickedBreadDOWN(object sender, EventArgs e)
+        {
+            s.LowerBread();
+        }
+        private void Button_ClickedBeerUP(object sender, EventArgs e)
+        {
+            s.RaiseBeer();
+        }
+        private void Button_ClickedBeerDOWN(object sender, EventArgs e)
+        {
+            s.LowerBeer();
+        }
+        private void Button_ClickedLemonadeUp(object sender, EventArgs e)
+        {
+            s.RaiseLemonades();
+        }
+        private void Button_ClickedLemonadeDOWN(object sender, EventArgs e)
+        {
+            s.Loweremonades();
+        }
+        #endregion
+
+        // Button Kaufen
+        private void Button_ClickedBuy(object sender, EventArgs e)
+        {
+
         }
     }
 }
